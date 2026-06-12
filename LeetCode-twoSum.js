4b2ba920ee2d638db83077493af696c2b5e5efc1
Jericho Morales <121097972+jecko04@users.noreply.github.com>
2026-06-12 09:56:59 +0000

    revise

--- LeetCode-twoSum.js
+++ LeetCode-twoSum.js
@@ -4,6 +4,7 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
@@ -12,3 +13,9 @@ var twoSum = function (nums, target) {
         }
     }
 };
+
+console.log (twoSum([2, 7, 11, 15], 9))
+
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
