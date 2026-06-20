f792bc94de7f0afeb811738dec007a08ae77f6f5
nicoledianecf <morales.jericho.05042002@gmail.com>
2026-06-20 12:44:28 +0800

    learning leet

--- LeetCode-RemoveElement.js
+++ LeetCode-RemoveElement.js
@@ -6,18 +6,22 @@
 var removeElement = function(nums, val) {
 
     let k = 0;
+    // const target = nums.indexOf(val);
+
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] != nums[i - 1]) {
-            val = nums[k];
-            k++
+        // const index = nums.indexOf(nums[i]);
+        // const index2 = nums.indexOf();
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
         }
-    }
-    // k = nums.filter(item => item != val)
-
-
+        // console.log("This is index: " + index);
+        // console.log("This is index2: " + nums[i]);
+    } 
+    
+    // console.log("This is target index: " + target);
     return k;
-
 };
 
 console.log(removeElement([3,2,2,3], 3));
