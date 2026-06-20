ce50a9108566f5e89f898950a39064c08299b769
Jericho Morales <121097972+jecko04@users.noreply.github.com>
2026-06-20 11:02:35 +0000

    learning leet

--- LeetCode-LengthOfLastWord.js
+++ LeetCode-LengthOfLastWord.js
@@ -6,8 +6,8 @@ var lengthOfLastWord = function(s) {
 
     const word = s.trim().split(" ");
     for (let i = 0; i < word.length; i++) {
-        console.log("this is for i loop: " + word.length);
-        // console.log("this is length: " + word[i].length);
+        // console.log("this is for i loop: " + word.at(-1).length);
+        return word.at(-1).length;
     }
 };
 
